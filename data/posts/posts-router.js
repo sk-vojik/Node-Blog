9cb375ebfd2612a1a4e4b6f6d25b3326cd0dcfd8
Scott Vojik <scott.vojik@gmail.com>
2019-02-13 14:09:40 -0800

    add and delete posts working

--- data/posts/posts-router.js
+++ data/posts/posts-router.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const db = require('../../data/helpers/postDb');
+const Posts = require('../../data/helpers/postDb');
 
 const router = express.Router();
 
@@ -8,7 +8,7 @@ const router = express.Router();
 //GET ALL POSTS
 
 router.get('/', (req, res) => {
-  db.get()
+  Posts.get()
     .then(posts => {
       res.status(200).json({ success: true, posts })
     })
@@ -20,7 +20,7 @@ router.get('/', (req, res) => {
 //GET BY ID
 
 router.get('/:id', (req, res) => {
-  db.getById(req.params.id)
+  Posts.getById(req.params.id)
     .then(post => {
       if (post) {
         return res.status(200).json({ success: true, post })
@@ -31,7 +31,46 @@ router.get('/:id', (req, res) => {
     .catch(error => {
       res.status(500).json({ sucess: false, error: "We could not retrieve this post at this time."})
     })
-  
 });
+
+
+//POST
+
+router.post('/', async (req, res) => {
+  if(!req.body.text) {
+    return res.status(400).json({ success: false, error: "Please provide text for the post."})
+  }
+  try {
+    const post = await Posts.insert(req.body);
+    res.status(201).json({ success: true, post })
+  } catch (error) {
+    res.status(500).json({ success: false, error: "There was an error while saving the post."})
+  }
+});
+
+//DELETE
+
+router.delete('/:id', async (req, res) => {
+  try {
+    const deleted = await Posts.remove(req.params.id);
+    if (deleted) {
+      return res.status(200).json({ success: true, deleted })
+    } else {
+     res.status(404).json({ success: false, error: "The post with the specified ID does not exist"})
+    }
+  } catch (error) {
+    res.status(500).json({ success: false, error: "The post could not be deleted at this time"})
+  }
+});
+
+
+
+
+
+
+
+
+
+
 
 module.exports = router;
