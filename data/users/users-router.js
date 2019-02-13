41957c0da99e067bedea84c9e575a9c4cd0503d8
Scott Vojik <scott.vojik@gmail.com>
2019-02-13 14:38:36 -0800

    able to get users by ID

--- data/users/users-router.js
+++ data/users/users-router.js
@@ -19,4 +19,22 @@ router.get('/', async (req, res) => {
   }
 });
 
+
+//GET USER BY ID
+
+router.get('/:id', async (req, res) => {
+  try {
+    const users = await Users.getById(req.params.id);
+    if (users) {
+      return res.status(200).json({ success: true, users});
+    } else {
+      res.status(404).json({ success: false, error: "The user with the specified ID does not exist"})
+    }
+  } catch (error) {
+    res.status(500).json({ success: false, error: "The user could not be retrieved at this time."})
+  }
+});
+
+
+
 module.exports = router;
